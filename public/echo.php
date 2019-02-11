da92a52feacce8c713b2c5b56d7472090f9256b5
muh005 <muh005@ucsd.edu>
2019-02-10 17:30:38 -0800

    env.php echo.php form.html

---
+++ public/echo.php
@@ -0,0 +1,24 @@
+<html>
+        <head>
+                <title>Echo Back</title>
+                <h1>Echo Back</h1>
+        </head>
+        <body>
+        <?php
+                $firstname = $_REQUEST['firstname'];
+                $lastname = $_REQUEST['lastname'];
+                $color = $_REQUEST['color'];
+                if ($color = "blue"){
+                        echo '<body style="background-color:blue">';
+                }
+                else if ($color = "yellow") {
+                        echo '<body style="background-color:yellow">';
+                }
+                else {
+                        echo '<body style="background-color:white">';
+                }
+                echo "Hello " . $firstname" " . $lastname . " from a Web app written in PHP on " . date("h:i:sa"). " time";
+
+        ?>
+        </body>
+</html>
