891257feb2223210d58c140c4c201869cf49ceee
muh005 <muh005@ucsd.edu>
2019-02-11 21:29:32 -0800

    session php

--- public/first.php
+++ public/first.php
@@ -1,10 +1,21 @@
-<?php
-session_start(); $_SESSION['fav_stooge']='Moe'; $_SESSION['num_stooges']=3;
-?>
-<html
+<html>
 <head>
-<title>Session Fun</title>
+<title>first_session</title>
+<h1 style="text-align: center;">First Session</h1>
 </head>
-<body>
-<h1>Just set some session values</h1>
+
+<form action="", method="post">
+    username: <input type=“text” name=“username” /><br>
+    <button type=“submit” value=“submit”>submit </button>
+</form>
+
 <a href="second.php">Next page</a>
+<body>
+<?php
+    session_start();
+    if(isset($_POST['submit'])){
+        $_SESSION['username']= $_REQUEST['username'];
+    }
+?>
+</body>
+</html>
--- public/second.php
+++ public/second.php
@@ -1,11 +1,25 @@
 <html
 <head>
 <title>Session Fun</title>
+<h1>Reading session variables</h1>
 </head>
 <body>
-<h1>Reading session variables</h1> <?php
-session_start();
-print "There are ".$_SESSION['num_stooges'] ." and my favorite is ". $_SESSION['fav_stooge'];
+
+<?php
+    session_start();
+    if (isset($_SESSION['username'])) {
+        print "Hi" . $_SESSION['username'] . "  nice to meet you!";
+    }
+    else{
+        print "Howdy stranger. Please tell me your name on page1!";
+    }
+
+
+    if(isset($_GET['logout'])) {
+        session_unset();
+    }
 ?>
-  </body>
-  </html>
+<br>
+<button href="?logout">Clear Session</button>
+</body>
+</html>
